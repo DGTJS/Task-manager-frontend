0a9c74788d29ed4e16af6353ccb70dc226fdd690
DGTJS <joaodiegom15@gmail.com>
2025-08-23 13:25:55 -0300

    style: add button checkbox

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,10 +1,31 @@
-const TaksItem = ({ task }) => {
+import Checkbox from "@mui/material/Checkbox";
+import { grey, green } from "@mui/material/colors";
+
+const TaskItem = ({ task }) => {
     return (
         <>
-            <h1>{task.description}</h1>
-            <div>{task.isCompleted ? "Completa" : "Incompleta"}</div>
+            <div className="taskItems">
+                <Checkbox
+                    checked={task.isCompleted}
+                    sx={{
+                        color: grey[50],
+                        "&.Mui-checked": {
+                            color: green[600],
+                        },
+                    }}
+                />
+                <h1
+                    style={{
+                        textDecoration: task.isCompleted
+                            ? "line-through"
+                            : "none",
+                    }}
+                >
+                    {task.description}
+                </h1>
+            </div>
         </>
     );
 };
 
-export default TaksItem;
+export default TaskItem;
