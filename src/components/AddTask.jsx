c50e541ccefa2a23961b108de960baef881b88b4
DGTJS <joaodiegom15@gmail.com>
2025-08-26 10:44:39 -0300

    feat: add react-toastify setu

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,20 +1,51 @@
 import "./AddTask.scss";
+import CustomButton from "./CustomButton";
 import CustomInput from "./CustomInput";
 import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+// import axios from "axios";
+import { toast, Flip, ToastContainer } from "react-toastify";
 
 const AddTask = () => {
     const [task, setTask] = useState("");
     const onChange = (e) => {
         setTask(e.target.value);
     };
+
+    const handleTaskAddition = async () => {
+        try {
+            if (task.length === 0) {
+                return toast.warn(
+                    "A tarefa precisa de uma descrição para ser adicionada",
+                    {
+                        position: "top-right",
+                        autoClose: 3000,
+                        hideProgressBar: false,
+                        closeOnClick: false,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "dark",
+                        transition: Flip,
+                    }
+                );
+            }
+        } catch (error) {}
+    };
     return (
-        <div className="add-task-container">
-            <CustomInput
-                label="Adicionar tarefa"
-                value={task}
-                onChange={onChange}
-            />
-        </div>
+        <>
+            <div className="add-task-container">
+                <CustomInput
+                    label="Adicionar tarefa"
+                    value={task}
+                    onChange={onChange}
+                />
+                <CustomButton onClick={handleTaskAddition}>
+                    <FaPlus size={14} />
+                </CustomButton>
+            </div>
+            <ToastContainer />
+        </>
     );
 };
 
