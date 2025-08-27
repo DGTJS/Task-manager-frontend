0aad302d3f6317b975e3ad500b4a82b16582da8f
DGTJS <joaodiegom15@gmail.com>
2025-08-27 12:15:31 -0300

    feat: add function to change a task status

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,7 +1,7 @@
 import "./TaskItem.scss";
 import { FaTrash } from "react-icons/fa";
 import axios from "axios";
-import { toast, Flip, ToastContainer } from "react-toastify";
+import { toast, Flip } from "react-toastify";
 
 const TaskItem = ({ task, fetchTasks }) => {
     const DeleteTaskItem = async () => {
@@ -11,6 +11,42 @@ const TaskItem = ({ task, fetchTasks }) => {
             );
             await fetchTasks();
             toast.success("A tarefa foi removida com sucesso", {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+                transition: Flip,
+            });
+        } catch (error) {
+            toast.warning("Algo deu errado.", {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+                transition: Flip,
+            });
+        }
+    };
+
+    const handleTaskCompletionChange = async (e) => {
+        try {
+            await axios.patch(
+                `https://task-manager-backend-71en.onrender.com/task/${task._id}`,
+                {
+                    isCompleted: e.target.checked,
+                }
+            );
+
+            await fetchTasks();
+            toast.success("A tarefa foi concluida com sucesso", {
                 position: "top-right",
                 autoClose: 5000,
                 hideProgressBar: false,
@@ -38,6 +74,7 @@ const TaskItem = ({ task, fetchTasks }) => {
                         <input
                             type="checkbox"
                             defaultChecked={task.isCompleted}
+                            onChange={(e) => handleTaskCompletionChange(e)}
                         />
                         {task.description}
                         <span
