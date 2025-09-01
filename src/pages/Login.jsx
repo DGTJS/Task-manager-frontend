6f8e16fb196e9bcd8add87150491861e3755e052
DGTJS <joaodiegom15@gmail.com>
2025-09-01 00:13:14 -0300

    feat: add Login router

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,8 +4,10 @@ import CustomButton from "../components/CustomButton";
 const Login = () => {
     return (
         <div className="login-container">
-            <img src={Logo} alt="Logo Task Manager Login" />
-            <CustomButton>Entrar</CustomButton>
+            <img src={Logo} alt="Logo Task Manager Login" width={"40%"} />
+            <div className="button-container">
+                <CustomButton>Entrar</CustomButton>
+            </div>
         </div>
     );
 };
