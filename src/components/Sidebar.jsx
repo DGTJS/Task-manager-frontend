9d1e75c34e748a89da96f7038ab98a295cc95cae
DGTJS <joaodiegom15@gmail.com>
2025-09-01 00:20:01 -0300

    feat: add navigation to home after Login and SignOut using useNavigate

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import "./Sidebar.scss";
 import Logo from "../assets/logo.png";
 import CustomButton from "./CustomButton";
 
 const Sidebar = () => {
+    const navigate = useNavigate();
+
+    const handleSignOutClick = () => {
+        navigate("/login");
+    };
+
     return (
         <div className="sidebar-container">
             <div className="logo">
                 <img src={Logo} alt="Logo Task Manager" width={"150"} />
             </div>
             <div className="sign-out">
-                <CustomButton>Sair</CustomButton>
+                <CustomButton onClick={handleSignOutClick}>Sair</CustomButton>
             </div>
         </div>
     );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import "./Login.scss";
 import Logo from "../assets/logo.png";
 import CustomButton from "../components/CustomButton";
 const Login = () => {
+    const navigate = useNavigate();
+
+    const handleSignInClick = () => {
+        navigate("/");
+    };
     return (
         <div className="login-container">
             <img src={Logo} alt="Logo Task Manager Login" width={"40%"} />
             <div className="button-container">
-                <CustomButton>Entrar</CustomButton>
+                <CustomButton onClick={handleSignInClick}>Entrar</CustomButton>
             </div>
         </div>
     );
